d0153d7957142625e8ca0bd4f96f123ae4dc0e4a
Igor <gintsc2@gmail.com>
2023-09-21 18:43:01 +0300

    Перенес с теелфона дз на комп

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using CSharpShellCore;
+
+namespace Homework__S1_2;
+
+public static class Program
+{
+	public static void Main()
+	{	
+     int[]Array = { 2, 3, 7};
+	 int Max = 0;
+	 for (int count=0;count<Array.Length; count++)
+	{
+		if (Array[count]>Max);
+		Max = Array[count];
+	}
+    Console.Write("Максимальнон число = ");
+	Console.WriteLine(Max);
+	}
+}
